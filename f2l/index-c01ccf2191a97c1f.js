ca9ecc2afecd6ad6a6eb3122b68b58bbe32b9178
akiradeveloper <akiradeveloper@users.noreply.github.com>
2021-10-09 14:37:21 +0000

    deploy: ec9c4abfd879ebffae03597daa011023618cf17e

--- f2l/index-c01ccf2191a97c1f.js
+++ f2l/index-c01ccf2191a97c1f.js
@@ -308,7 +308,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-613131874a0b9092_bg.wasm', import.meta.url);
+        input = new URL('index-c01ccf2191a97c1f_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
