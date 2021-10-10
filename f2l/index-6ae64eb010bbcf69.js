c7bbd9c65ae00886cc572922501d1d8337af4aeb
akiradeveloper <akiradeveloper@users.noreply.github.com>
2021-10-10 18:54:08 +0000

    deploy: 5bb92614f58319462baeb6bfaabece18a84093f9

--- f2l/index-6ae64eb010bbcf69.js
+++ f2l/index-6ae64eb010bbcf69.js
@@ -308,7 +308,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-76731b6762fad01_bg.wasm', import.meta.url);
+        input = new URL('index-6ae64eb010bbcf69_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
